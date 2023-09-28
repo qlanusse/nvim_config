2bc1493b80a3934633a312cabb68d2d356707050
qlanusse <qlanusse@gmail.com>
2023-09-28 14:59:17 +0200

    Add sh files indent

---
+++ ftplugin/sh.lua
@@ -0,0 +1,11 @@
+local set = vim.opt_local
+
+-- Tabulation config
+set.shiftround = true
+set.smartindent = true
+set.autoindent = true
+set.expandtab = true
+-- Filetype configs
+set.tabstop = 2
+set.shiftwidth = 2
+set.softtabstop = 2
